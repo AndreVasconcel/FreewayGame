ed1a15da7d797652a9bfffca175c86daf3293089
André Vasconcelos <165220472+AndreVasconcel@users.noreply.github.com>
2025-01-09 21:37:26 -0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+FreewayGame é um jogo viciante inspirado no clássico estilo de arcade, onde os jogadores controlam
+um personagem que deve atravessar uma estrada movimentada sem ser atropelado. Este projeto foi desenvolvido
+utilizando a biblioteca p5.jspara fornecer uma experiência interativa e divertida.
---
+++ imagens.js
@@ -0,0 +1,16 @@
+// imagens do jogo
+
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+function preload(){
+    imagemDaEstrada = loadImage("imgs/estrada.png", );
+    imagemDoAtor = loadImage("imgs/ator-1.png");
+    imagemCarro = loadImage("imgs/carro-1.png");
+    imagemCarro2 = loadImage("imgs/carro-2.png");
+    imagemCarro3 = loadImage("imgs/carro-3.png");  
+    imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+}  
